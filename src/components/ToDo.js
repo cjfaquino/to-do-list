86fc0d2b7fbf985c765c32c58316e40f13654d89
Christian Aquino <cjfaquino@gmail.com>
2022-09-23 21:59:28 -0700

    add ToDo class

---
+++ src/components/ToDo.js
@@ -0,0 +1,24 @@
+export class ToDo {
+  constructor(name, dueDate, desc = '') {
+    this.name = name;
+    this.dueDate = dueDate;
+    this.desc = desc;
+    this.completed = false;
+  }
+
+  setName(value) {
+    this.name = value;
+  }
+
+  setDate(value) {
+    this.dueDate = value;
+  }
+
+  setDesc(value) {
+    this.desc = value;
+  }
+
+  toggleComp() {
+    this.completed = !this.completed;
+  }
+}
