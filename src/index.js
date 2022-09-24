06014f9dbe1ab95ad0b1a6ea96a5999703cb8f8a
Christian Aquino <cjfaquino@gmail.com>
2022-09-23 22:01:19 -0700

    add placeholder data

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './style.css';
 
+import { List } from './components/List';
+import { ToDo } from './components/ToDo';
+
+let projects = [];
+
+
+//test data
+const list1 = new List('Project')
+const test1 = new ToDo('test1', new Date())
+const test2 = new ToDo('test2', new Date())
+
+projects.push(list1)
+list1.list.push(test1)
+list1.list.push(test2)
+test1.toggleComp();
