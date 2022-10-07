e8bffc806898b485415fa349c50f36f71460fa50
Christian Aquino <cjfaquino@gmail.com>
2022-10-06 20:52:17 -0700

    add delete button & push to notesArr

--- src/components/createNewNoteDOM.js
+++ src/components/createNewNoteDOM.js
@@ -1,4 +1,5 @@
-import { listView } from "..";
+import { listView, notesArr } from "..";
+import { renderNotes } from "./renderItems";
 
 export const addNoteBtn = document.createElement('button');
 addNoteBtn.textContent = 'Add';
@@ -9,10 +10,27 @@ addNoteBtn.addEventListener('click', createNewNoteDOM)
 function createNewNoteDOM() {
   const div = document.createElement('div');
   const textArea = document.createElement('textarea');
+  const del = document.createElement('div');
 
   div.classList.add('noteItem');
   textArea.classList.add('noteText');
+  del.classList.add('deleteNote');
 
-  div.append(textArea)
+  del.innerHTML = '<i class="fa-solid fa-delete-left"></i>';
+
+  div.append(textArea, del)
   listView.append(div)
+
+  del.addEventListener('click', deleteNote)
+
+  addToNotesArr(div)
+
+  function deleteNote() {
+    notesArr.splice(notesArr.indexOf(div), 1)
+    renderNotes();
+  }
+}
+
+function addToNotesArr(el) {
+  notesArr.push(el)
 }
