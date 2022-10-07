b4b01211441ca14d5556f31b7a7a8f282f841c53
Christian Aquino <cjfaquino@gmail.com>
2022-10-06 18:35:45 -0700

    update methods into functions

--- src/components/test.js
+++ src/components/test.js
@@ -1,4 +1,4 @@
-import { ToDo } from './ToDo';
+import { ToDo, toggleToDoCompleted } from './ToDo';
 import { createNewProject } from "./createNewInput";
 
 export const test = () => {
@@ -7,28 +7,34 @@ export const test = () => {
   
   const todo0 = new ToDo('Create delete button', '2022-09-29', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam libero justo laoreet sit amet cursus. Amet est placerat in egestas erat.');
   project1.list.push(todo0);
-  todo0.toggleComp();
+  // todo0.toggleComp();
+  toggleToDoCompleted(todo0);
 
   const todo = new ToDo('Create a way to update values', '2022-09-29');
   project1.list.push(todo);
-  todo.toggleComp();
+  // todo.toggleComp();
+  toggleToDoCompleted(todo)
 
   const todo10 = new ToDo('Expand for description', '2022-09-30', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam libero justo laoreet sit amet cursus. Amet est placerat in egestas erat.');
   project1.list.push(todo10);
-  todo10.toggleComp();
+  // todo10.toggleComp();
+  toggleToDoCompleted(todo10);
 
 
   const todo9 = new ToDo('Create currently selected project function', '2022-10-01');
   project1.list.push(todo9);
-  todo9.toggleComp();
+  // todo9.toggleComp();
+  toggleToDoCompleted(todo9)
 
   const todo1 = new ToDo('Sort dates', '2022-10-01');
   project1.list.push(todo1);
-  todo1.toggleComp()
+  // todo1.toggleComp()
+  toggleToDoCompleted(todo1)
 
   const todo8 = new ToDo('Change date format', '2022-10-01');
   project1.list.push(todo8);
-  todo8.toggleComp();
+  // todo8.toggleComp();
+  toggleToDoCompleted(todo8)
 
   const todo2 = new ToDo('Filter by today', '2022-10-01');
   project1.list.push(todo2);
@@ -44,12 +50,14 @@ export const test = () => {
 
   const todo5 = new ToDo('Do laundry', new Date().toISOString().split('T')[0]);
   project2.list.push(todo5);
-  todo5.toggleComp();
+  // todo5.toggleComp();
+  toggleToDoCompleted(todo5)
 
   const todo6 = new ToDo('Buy groceries', new Date().toISOString().split('T')[0]);
   project2.list.push(todo6);
 
   const todo11 = new ToDo('Take out the trash', new Date().toISOString().split('T')[0]);
   project2.list.push(todo11);
-  todo11.toggleComp();
+  // todo11.toggleComp();
+  toggleToDoCompleted(todo11)
 };
