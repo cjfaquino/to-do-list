48462864bc968aa7ee3e00785722f7bbe1186d18
Christian Aquino <cjfaquino@gmail.com>
2022-10-06 19:26:11 -0700

    add createNewNoteDOM

--- src/components/createNewNoteDOM.js
+++ src/components/createNewNoteDOM.js
@@ -1,5 +1,18 @@
+import { listView } from "..";
 
 export const addNoteBtn = document.createElement('button');
 addNoteBtn.textContent = 'Add';
 addNoteBtn.classList.add('newNote')
 
+addNoteBtn.addEventListener('click', createNewNoteDOM)
+
+function createNewNoteDOM() {
+  const div = document.createElement('div');
+  const textArea = document.createElement('textarea');
+
+  div.classList.add('noteItem');
+  textArea.classList.add('noteText');
+
+  div.append(textArea)
+  listView.append(div)
+}
