ddb1cfa1109d2a55867e4b086c2d7f9f3cb6b0f7
Christian Aquino <cjfaquino@gmail.com>
2022-11-01 22:55:32 -0700

    change webpack config & install devDependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
+  entry: {
+    index: './src/index.js',
   },
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'Weather App',
+      metaDesc: 'Weather app based on OpenWeather API',
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
 };
